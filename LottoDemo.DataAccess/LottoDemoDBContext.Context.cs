3d43a1d652aeb8397d95f683941b5f16dfc6bd30
zlatko.bandrov@abv.bg <zlatko.bandrov@abv.bg>
2016-12-27 18:48:29 +0200

    Commit

--- LottoDemo.DataAccess/LottoDemoDBContext.Context.cs
+++ LottoDemo.DataAccess/LottoDemoDBContext.Context.cs
@@ -12,12 +12,15 @@ namespace LottoDemo.DataAccess
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
-    public partial class LotteryDemoDBEntities1 : DbContext
+    public partial class LotteryDemoDBEntities : DbContext
     {
-        public LotteryDemoDBEntities1()
-            : base("name=LotteryDemoDBEntities1")
+        public LotteryDemoDBEntities()
+            : base("name=LotteryDemoDBEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -34,5 +37,46 @@ namespace LottoDemo.DataAccess
         public virtual DbSet<LottoDrawing> LottoDrawings { get; set; }
         public virtual DbSet<LottoTicket> LottoTickets { get; set; }
         public virtual DbSet<User> Users { get; set; }
+    
+        public virtual int CreateNewLottoDrawing(Nullable<int> lotteryGame, Nullable<System.DateTime> drawTime, Nullable<byte> numberOne, Nullable<byte> numberTwo, Nullable<byte> numberThree, Nullable<byte> numberFour, Nullable<byte> numberFive, Nullable<byte> numberSix, Nullable<byte> numberSeven)
+        {
+            var lotteryGameParameter = lotteryGame.HasValue ?
+                new ObjectParameter("LotteryGame", lotteryGame) :
+                new ObjectParameter("LotteryGame", typeof(int));
+    
+            var drawTimeParameter = drawTime.HasValue ?
+                new ObjectParameter("DrawTime", drawTime) :
+                new ObjectParameter("DrawTime", typeof(System.DateTime));
+    
+            var numberOneParameter = numberOne.HasValue ?
+                new ObjectParameter("NumberOne", numberOne) :
+                new ObjectParameter("NumberOne", typeof(byte));
+    
+            var numberTwoParameter = numberTwo.HasValue ?
+                new ObjectParameter("NumberTwo", numberTwo) :
+                new ObjectParameter("NumberTwo", typeof(byte));
+    
+            var numberThreeParameter = numberThree.HasValue ?
+                new ObjectParameter("NumberThree", numberThree) :
+                new ObjectParameter("NumberThree", typeof(byte));
+    
+            var numberFourParameter = numberFour.HasValue ?
+                new ObjectParameter("NumberFour", numberFour) :
+                new ObjectParameter("NumberFour", typeof(byte));
+    
+            var numberFiveParameter = numberFive.HasValue ?
+                new ObjectParameter("NumberFive", numberFive) :
+                new ObjectParameter("NumberFive", typeof(byte));
+    
+            var numberSixParameter = numberSix.HasValue ?
+                new ObjectParameter("NumberSix", numberSix) :
+                new ObjectParameter("NumberSix", typeof(byte));
+    
+            var numberSevenParameter = numberSeven.HasValue ?
+                new ObjectParameter("NumberSeven", numberSeven) :
+                new ObjectParameter("NumberSeven", typeof(byte));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("CreateNewLottoDrawing", lotteryGameParameter, drawTimeParameter, numberOneParameter, numberTwoParameter, numberThreeParameter, numberFourParameter, numberFiveParameter, numberSixParameter, numberSevenParameter);
+        }
     }
 }
---
+++ LottoDemo.DataAccess/Repositories/Generic/GenericDataRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LottoDemo.DataAccess.Repositories.Generic
+{
+    public class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
+    {
+        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            using (var context = new LotteryDemoDBEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .ToList<T>();
+            }
+            return list;
+        }
+
+        public virtual IList<T> GetList(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            using (var context = new LotteryDemoDBEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .Where(where)
+                    .ToList<T>();
+            }
+            return list;
+        }
+
+        public virtual T GetSingle(Func<T, bool> where,
+             params Expression<Func<T, object>>[] navigationProperties)
+        {
+            T item = null;
+            using (var context = new LotteryDemoDBEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                item = dbQuery
+                    .AsNoTracking() //Don't track any changes for the selected item
+                    .FirstOrDefault(where); //Apply where clause
+            }
+            return item;
+        }
+
+        public virtual void Update(params T[] items)
+        {
+            using (var context = new LotteryDemoDBEntities())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public virtual void Remove(params T[] items)
+        {
+            using (var context = new LotteryDemoDBEntities())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void Add(params T[] items)
+        {
+            dbSet.Add(entity);
+        }
+    }
+}
