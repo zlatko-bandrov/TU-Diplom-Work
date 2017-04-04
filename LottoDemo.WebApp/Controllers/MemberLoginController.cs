25cd4852ae65211a822449b3545e4cbd85d69e4d
zlatko.bandrov@abv.bg <zlatko.bandrov@abv.bg>
2017-04-04 17:33:59 +0300

    Commit

---
+++ LottoDemo.WebApp/Controllers/MemberLoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using LottoDemo.WebApp.Models.Membership;
+using System.Web.Security;
+
+namespace LottoDemo.WebApp.Controllers
+{
+    public class MemberLoginController : SurfaceController
+    {
+        public ActionResult RenderLoginDialog()
+        {
+            var model = new LoginFromViewModel();
+
+            return PartialView("LoginForm/_LoginFormDialog", model);
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView();
+        }
+
+        public ActionResult RenderMemberMenuActions()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginFromViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.MemberEmail, model.MemberPassword))
+                {
+                    FormsAuthentication.SetAuthCookie(model.MemberEmail, false);
+                    UrlHelper urlHelper = new UrlHelper(HttpContext.Request.RequestContext);
+
+                    return Redirect("/");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            FormsAuthentication.SignOut();
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ LottoDemo.WebApp/Models/Membership/LoginFromViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LottoDemo.WebApp.Models.Membership
+{
+    public class LoginFromViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Country { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string MemberEmail { get; set; }
+
+        public string MemberPassword { get; set; }
+    }
+}
